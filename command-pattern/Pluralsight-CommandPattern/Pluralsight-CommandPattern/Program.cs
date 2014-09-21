67cac0c4bb5bec6cc114cea2afbdcab1e8800363
edalkiran <erdal.dalkiran@hepsiburada.com>
2014-09-21 14:41:58 +0300

    #000 ed initial commit

---
+++ command-pattern/Pluralsight-CommandPattern/Pluralsight-CommandPattern/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Pluralsight_CommandPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProgramWithoutPattern.Run(args);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ command-pattern/Pluralsight-CommandPattern/Pluralsight-CommandPattern/ProgramWithoutPattern.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Pluralsight_CommandPattern
+{
+    public class ProgramWithoutPattern
+    {
+        public static void Run(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                PrintUsage();
+                return;
+            }
+
+            var processor = new CommandProcessor();
+            processor.ExecuteCommand(args);
+        }
+
+        private static void PrintUsage()
+        {
+            Console.WriteLine("Usage: LoggingDemo CommandName Arguments");
+            Console.WriteLine("Commands:");
+            Console.WriteLine("   UpdateQuantity number");
+            Console.WriteLine("   CreateOrder");
+            Console.WriteLine("   ShipOrder");
+        }
+    }
+}
